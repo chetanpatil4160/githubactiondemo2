129b858ebac7eb168f27ddfd5bcf17f918ccdfd1
chetan patil <chetanpatil4160@gmail.com>
2025-11-06 19:41:23 +0530

    test1

---
+++ test_calc.py
@@ -0,0 +1,19 @@
+import pytest
+
+from calc import add,sub,mul,div
+
+def test_add():
+    # in unit test caess we compare actual o/p vs expected o/p
+    assert add(2,3) == 5
+
+def test_sub():
+    # in unit test caess we compare actual o/p vs expected o/p
+    assert add(3,1) == 2
+
+def test_mul():
+    # in unit test caess we compare actual o/p vs expected o/p
+    assert add(4,2) == 8
+
+def test_div():
+    # in unit test caess we compare actual o/p vs expected o/p
+    assert add(4,2) == 2
